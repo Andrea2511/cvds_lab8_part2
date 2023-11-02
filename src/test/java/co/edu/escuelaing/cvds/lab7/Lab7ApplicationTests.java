df7568f0c59b78c37ba4650a323898faf1e29af6
Andrea Torres <andrea.torres-trmail.escuelaing.edu.co>
2023-11-02 18:37:02 -0500

    parcial practico

--- src/test/java/co/edu/escuelaing/cvds/lab7/Lab7ApplicationTests.java
+++ src/test/java/co/edu/escuelaing/cvds/lab7/Lab7ApplicationTests.java
@@ -3,7 +3,6 @@ package co.edu.escuelaing.cvds.lab7;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class Lab7ApplicationTests {
 
 
